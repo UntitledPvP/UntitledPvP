963f5b0769e03fcee70ce79e3ab1ff911bf76be9
UntitledPvP <42239886+UntitledPvP@users.noreply.github.com>
2018-08-09 18:06:51 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -197,13 +197,12 @@ client.on('message', message => {
 }
 });
 
-client.on("message", (message) => {
-    
+client.on('message', message => {
     if (message.content.startsWith("%new")) {
         const reason = message.content.split(" ").slice(1).join(" ");
         if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
         if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`You already have a ticket open.`);
-        message.guild.createChannel(`ticket-${message.author.id}`, "text").then(c => {
+        message.guild.createChannel(`ticket-${message.author.discriminator}`, "text").then(c => {
             let role = message.guild.roles.find("name", "Support Team");
             let role2 = message.guild.roles.find("name", "@everyone");
             c.overwritePermissions(role, {
