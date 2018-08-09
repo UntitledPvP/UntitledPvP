6a419e900a06d1ee2bb9626a6f3e9f84b5a605fc
UntitledPvP <42239886+UntitledPvP@users.noreply.github.com>
2018-08-09 18:01:46 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -197,12 +197,13 @@ client.on('message', message => {
 }
 });
 
-client.on('message', message => {
+client.on("message", (message) => {
+    
     if (message.content.startsWith("%new")) {
         const reason = message.content.split(" ").slice(1).join(" ");
         if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
         if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`You already have a ticket open.`);
-        message.guild.createChannel(`ticket-${message.author.discriminator}`, "text").then(c => {
+        message.guild.createChannel(`ticket-${message.author.id}`, "text").then(c => {
             let role = message.guild.roles.find("name", "Support Team");
             let role2 = message.guild.roles.find("name", "@everyone");
             c.overwritePermissions(role, {
@@ -232,7 +233,7 @@ client.on('message', message => {
     if (message.content.startsWith("%close")) {
         if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
 
-        message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`.close\`. This will time out in 10 seconds and be cancelled.`)
+        message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`%close\`. This will time out in 10 seconds and be cancelled.`)
             .then((m) => {
                 message.channel.awaitMessages(response => response.content === '%close', {
                         max: 1,
